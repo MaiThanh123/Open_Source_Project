252c527c3d692931148bbd8462aeb94e12625b2b
Taeyoung Kim <92428182+Whatisyouremergency@users.noreply.github.com>
2025-06-16 16:51:19 +0900

    Delete backend directory

--- backend/routes/fighters.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getFighterByName, getFightersByWeightClass } = require('../controllers/fighterController');
-
-router.get('/:name', getFighterByName);
-router.get('/weight/:weight_class', getFightersByWeightClass);
-
-module.exports = fighterRoutes;
